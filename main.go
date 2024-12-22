04c7856bee9691574ab87f7cd797fbef88f074e4
alshahadath <abdullah.alshahadath@vstinc.com>
2024-12-22 16:50:34 +0600

    implement the gin in basic step

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	app := gin.Default()
+	app.Use(gin.Recovery())
+	app.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Server is running fine!",
+			"statusCode":200,
+		})
+	})
 	fmt.Println("Go app started successfully")
+
+	app.Run()
 }
